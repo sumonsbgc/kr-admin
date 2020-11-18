3753356c3a0dd85a9b87c3c317d445fc85c3903f
sumonsbgc <sumonsbgc@gmail.com>
2020-11-18 14:44:11 +0600

    fixed responsive issue

--- gulpfile.js
+++ gulpfile.js
@@ -3,17 +3,38 @@ const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
+const uglify = require('gulp-uglify');
+const concat = require('gulp-concat');
 const sourcemaps = require('gulp-sourcemaps');
 
-//const browserSync = require('browser-sync').create();
-//sass.compiler = require('node-sass');
-
 function style() {
-    return src('./assets/scss/*.scss')
+    return src([
+        './assets/css/select2.min.css',
+        './assets/scss/*.scss'
+    ])
         .pipe(sourcemaps.init())
         .pipe(sass())
         .pipe(postcss([autoprefixer(), cssnano()]))
+        .pipe(concat('style.css'))
         .pipe(sourcemaps.write("."))
         .pipe(dest('./'));
 }
-exports.default = style;
+
+function js() {
+    return src([
+        'node_modules/jquery/dist/jquery.min.js',
+        './assets/js/select2.full.min.js',
+        './assets/js/sweetalert.min.js',
+    ])
+        .pipe(sourcemaps.init())
+        .pipe(uglify())
+        .pipe(concat('index.js'))
+        .pipe(sourcemaps.write('.'))
+        .pipe(dest('./assets/js/'));
+}
+
+
+exports.style = style;
+exports.js = js;
+
+exports.default = series(style, js)
